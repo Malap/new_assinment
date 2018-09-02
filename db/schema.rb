0f7d5e22e77979b801d6844ed321b904315262fd
venkat <malapati532@gmail.com>
2018-09-02 13:53:51 +0530

    added some rake tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180901170441) do
+ActiveRecord::Schema.define(version: 20180902064700) do
 
   create_table "books", force: :cascade do |t|
     t.string  "name"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20180901170441) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "active"
   end
 
 end
